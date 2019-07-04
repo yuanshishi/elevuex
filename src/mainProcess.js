af89480824e360bcf5aa802c7ca29a3d871b799c
ys <275948172@qq.com>
2019-07-04 23:55:51 +0800

    问题修复

--- src/mainProcess.js
+++ src/mainProcess.js
@@ -13,15 +13,16 @@ class mainProcess {
     //初始化
     init() {
         ipcMain.on(CONNECT, event => {
+            let id=event.sender.id
             this.List.set(event.sender.id, event.sender)
             event.sender.webContents.send(INIT, this.store.state)
             event.sender.on("destroyed", () => {
-                this.List.delete(event.sender.id)
+                this.List.delete(id)
             })
         })
 
-        ipcMain.on(SENDDATA, (event, mutation)  => {
-            this.store.commit(mutation)
+        ipcMain.on(SENDDATA, (event, mutation) => {
+            this.store.dispatch(mutation.type,mutation.payload)
             this.noticeRenderers(mutation)
         })
     }
@@ -29,7 +30,7 @@ class mainProcess {
     //通知渲染进程
     noticeRenderers(payload) {
         this.List.forEach((sender) => {
-            if (sender.isDestroyed()) {
+            if (!sender.isDestroyed()) {
                 sender.webContents.send(SENDDATA, payload)
             }
         })
--- src/rendererProcess.js
+++ src/rendererProcess.js
@@ -7,17 +7,21 @@ const SENDDATA = "ys_senddata"
 class rendererProcess {
     constructor(store) {
         this.store = store
-        this.store.originCommit = this.store.commit
-        this.init() 
+        this.store.originDispatch = this.store.dispatch
+        this.init()
     }
     //初始化
     init() {
-        this.store.commit = (event, mutation) => {
-            this.noticeMain(mutation)
+       
+        this.store.commit =  () => {
+            throw new Error(`vuex请使用dispatch.`)
+          }
+        this.store.dispatch = (type, payload) => {
+            this.noticeMain({type, payload})
         }
         ipcRenderer.send(CONNECT)
         ipcRenderer.on(SENDDATA, (event, mutation) => {
-            this.updateVuex(event, mutation)
+            this.updateVuex( mutation)
         })
         ipcRenderer.on(INIT, (e,state) => {
             this.store.replaceState(state)
@@ -30,8 +34,8 @@ class rendererProcess {
     }
 
     //更新vuex
-    updateVuex(event, mutation) {
-        this.store.originCommit(event, mutation)
+    updateVuex(mutation) {
+        this.store.originDispatch(mutation.type,mutation.payload)
     }
 
 }
